98bb46780a63e807f321a9f8ae77b7db2f919553
Ktulchu <roma442006@yandex.ru>
2022-07-03 10:31:16 +0300

    Update Chartwidget.php

--- src/Chartwidget.php
+++ src/Chartwidget.php
@@ -3,67 +3,62 @@
 namespace rkdev\chart;
 
 use rkdev\chart\Chart;
+use yii\web\View;
+use rkdev\chart\ChartAssetsBundle;
+use yii\bootstrap4\ActiveForm;
 /**
  * This is just an example.
  */
 class Chartwidget extends \yii\base\Widget
 {
     public $options;
+		
+	public $sourcePath = '@vendor/ktulchu/yii2-chart/assets/';
 	
 	public function run()
     {
 		
+		$view = $this->getView();
+		
+		ChartAssetsBundle::register($view);
+		
 		$model = new Chart($this->options);
-		
-		$json = json_encode($model->getChart());
-		
-		$chart = <<< JS
-		  function Aply() {		
-			  $.ajax({
-				  type: 'get',
-				  url: '/',
-				  data: $('#filter-form').serialize(),
-				  type: 'POST',
-				  dataType: 'json',
-				  success: function(json) {
-					  if (typeof json['total'] == 'undefined') { return false; }
-					  
-					  var option = {	
-						shadowSize: 0,
-						colors: ['#9FD5F1', '#1065D2'],
-						bars: { 
-							show: true,
-							fill: true,
-							lineWidth: 1
-						},
-						grid: {
-							backgroundColor: '#FFFFFF',
-							hoverable: true
-						},
-						points: {
-							show: false
-						},
-						xaxis: {
-							show: true,
-							ticks: json['xaxis']
-						}
-					}
 					
-					$.plot('#chart-request', [json['total'], json['poprequests']], option);
-				  },
-					error: function(xhr, ajaxOptions, thrownError) {
-				   alert(thrownError);
+		$json = json_encode($model->getChart());
+						
+		$chart = <<< JS
+			var json = $json;
+			if (typeof json['total'] == 'undefined') { return false; }	  
+			  var option = {	
+				shadowSize: 0,
+				colors: ['#9FD5F1', '#1065D2'],
+				bars: { 
+					show: true,
+					fill: true,
+					lineWidth: 1
+				},
+				grid: {
+					backgroundColor: '#FFFFFF',
+					hoverable: true
+				},
+				points: {
+					show: false
+				},
+				xaxis: {
+					show: true,
+					ticks: json['xaxis']
 				}
-			  });
-		  };
-		  
-		  $('select, input').on('change', function(){ 
-			Aply();
-		  });
-		  Aply();
+			}
+			$.plot('#chart-request', [json['total'], json['poprequests']], option);
 		JS;
-		$this->registerJs($chart, yii\web\View::POS_READY);
 
-		return '<div id="chart"></div>';
+		$this->getView()->registerJs($chart);
+		
+		$html = '<div id="chart" style="height: 260px; border: 1px solid;"></div>';
+		$form = ActiveForm::begin(['id' => 'filter-form']);
+		
+		ActiveForm::end();
+
+		return $html;
     }
 }
