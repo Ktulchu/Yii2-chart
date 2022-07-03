288e966c5f4121a1b30a8092e2f2c332031ac649
Ktulchu <roma442006@yandex.ru>
2022-07-03 10:40:17 +0300

    Update Chartwidget.php

--- src/Chartwidget.php
+++ src/Chartwidget.php
@@ -7,7 +7,7 @@ use yii\web\View;
 use rkdev\chart\ChartAssetsBundle;
 use yii\bootstrap4\ActiveForm;
 /**
- * This is just an example.
+ * This is plot chart.
  */
 class Chartwidget extends \yii\base\Widget
 {
@@ -23,6 +23,8 @@ class Chartwidget extends \yii\base\Widget
 		$model = new Chart($this->options);
 					
 		$json = json_encode($model->getChart());
+		
+		$html = '<div id="chart" style="height: 260px;"></div>';
 						
 		$chart = <<< JS
 			var json = $json;
@@ -47,15 +49,10 @@ class Chartwidget extends \yii\base\Widget
 					ticks: json['xaxis']
 				}
 			}
-			$.plot('#chart-request', [json['total'], json['poprequests']], option);
+			$.plot('#chart', [json['total'], json['poprequests']], option);
 		JS;
 
 		$this->getView()->registerJs($chart);
-		
-		$html = '<div id="chart" style="height: 260px; border: 1px solid;"></div>';
-		$form = ActiveForm::begin(['id' => 'filter-form']);
-		
-		ActiveForm::end();
 
 		return $html;
     }
